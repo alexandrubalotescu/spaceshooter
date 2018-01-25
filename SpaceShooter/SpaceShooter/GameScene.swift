517eff9bd63943cd8b1d9dcda9e3883b6f9a9614
Alexandru Constantin Balotescu <alexandru.balotescu@gmail.com>
2018-01-25 17:12:32 +0100

    Am adaugat un font
    dada

--- SpaceShooter/SpaceShooter/GameScene.swift
+++ SpaceShooter/SpaceShooter/GameScene.swift
@@ -121,7 +121,7 @@ class GameScene: SKScene {
     func random() -> CGFloat {
         return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
     }
-    func random(min min: CGFloat, max: CGFloat) -> CGFloat {
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
         return random() * (max - min) + min
     }
     
